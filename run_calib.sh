c92e18f5daab3952ba833cc2fe862381926c8d0e
lingqiz <zlqzcc@gmail.com>
2024-12-10 17:05:26 -0500

    bug fix

--- run_calib.sh
+++ run_calib.sh
@@ -5,5 +5,6 @@ input_file="file_names.txt"
 
 # Read the file line by line
 while IFS= read -r line; do
-    poetry run python3 "$line"            # Use the line as an argument to the command
+    command="poetry run python3 run_calib.py $line"
+    eval "$command"
 done < "$input_file"
