d0370a98bbaf1fd4f8b11c694e9c9934617a5eba
lingqiz <zlqzcc@gmail.com>
2024-12-11 23:45:04 -0500

    change output path

--- flow/constants.py
+++ flow/constants.py
@@ -1,3 +1,5 @@
+import os 
+
 ZABER_BASE = '/groups/dennis/dennislab/data/processed_data'
 HS_BASE = '/groups/dennis/dennislab/data/hs_cam'
-TMP_PATH = './.tmp'
+TMP_PATH = os.path.expanduser('~/.tmp/hs_align')
