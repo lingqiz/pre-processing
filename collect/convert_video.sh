c762313d5d8c128cbfa7f498c949d3d79d8b520b
lingqiz <zlqzcc@gmail.com>
2025-09-22 18:17:20 -0400

    minor update

--- collect/convert_video.sh
+++ collect/convert_video.sh
@@ -11,10 +11,12 @@ filename=$(basename "$input_file")
 name="${filename%.*}"
 output_file="${input_dir}/${name}_cmp.avi"
 
-ssh -o "StrictHostKeyChecking no" -t login1.int.janelia.org \
+output=$(ssh -o "StrictHostKeyChecking no" -t login1.int.janelia.org \
   "bsub -J convert_mov -o /dev/null -n 8 'ffmpeg -i \"$input_file\" \
     -vf \"normalize=smoothing=60:strength=0.8,format=gray\" \
     -pix_fmt yuv420p \
     -c:v libx264 -crf 26 -preset medium -movflags +faststart -an \
-    \"$output_file\"'"
+    \"$output_file\"'" 2>&1)
+
+echo "Submitted job to cluster for converting $filename"
 
