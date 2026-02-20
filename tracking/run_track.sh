4cff767b4a332e11c77aca9ea782b858cb053804
lingqiz <zlqzcc@gmail.com>
2026-02-20 13:48:48 -0500

    switch to NVIDIA L4

--- tracking/run_track.sh
+++ tracking/run_track.sh
@@ -3,7 +3,7 @@ VIDEO_NAME=$1
 echo "Tracking video: ${VIDEO_NAME}"
 
 ssh -o "StrictHostKeyChecking no" -t login1.int.janelia.org \
-    "bsub -n 8 -gpu \"num=1\" -q gpu_a100 -W 24:00 -o \"/groups/zhang/home/zhangl5/Emily/Video_Process/.apt/track/track_${VIDEO_NAME}.log\" \
+    "bsub -n 8 -gpu \"num=1\" -q gpu_l4 -W 24:00 -o \"/groups/zhang/home/zhangl5/Emily/Video_Process/.apt/track/track_${VIDEO_NAME}.log\" \
     -R\"affinity[core(1)]\" -J DLC_Track_${VIDEO_NAME} \
     \"singularity exec --nv -B \\\"/groups\\\" -B \\\"/nrs\\\"  \\\"/groups/branson/bransonlab/apt/sif/apt_20230427_tf211_pytorch113_ampere.sif\\\" \
     bash -c \\\"TORCH_HOME='/groups/zhang/home/zhangl5/.apt/torch' python '/groups/zhang/home/zhangl5/APT/deepnet/APT_interface.py' \
